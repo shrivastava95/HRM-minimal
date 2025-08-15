3d410fc596708d2941b459cf2cd884420234fafb
Ishaan Shrivastava <ishaan@metafusion.ai>
2025-08-15 22:29:34 +0530

    add incomplete model and config

---
+++ config.py
@@ -0,0 +1,36 @@
+from types import SimpleNamespace
+
+config = SimpleNamespace(
+    causal             = False   ,
+    norm_order         = 'post'  , # config.norm_order: 'pre' | 'post'
+    norm_kind          = 'rms'   , # config.norm_kind : 'rms' | 'layer'
+    activation         = 'swiglu', # config.activation: 'swiglu' | 'gelu' | 'silu'
+    d_model            = 512     ,
+    num_heads          = 8       ,
+    num_puzzle_embs    = 1       ,
+    vocab_size         = 11      ,
+    L_depth            = 4       ,
+    H_depth            = 4       ,
+    L_cycles           = 2       ,
+    H_cycles           = 2       ,
+    M_min              = 2       ,
+    M_max              = 16      ,
+    max_seq_len        = 82      ,
+    
+    epochs             = 20000   ,
+    eval_interval      = 2000    ,
+    batch_size         = 32      ,
+    lr                 = 2e-5    ,
+    lr_warmup_steps    = 500     ,
+    wd                 = 1e0     ,
+    beta1              = 0.9     ,
+    beta2              = 0.95    ,
+    puzzle_emb_lr      = 2e-4    ,
+    puzzle_emb_wd      = 1e0     ,
+    rope_theta         = 10000.0 ,
+    layer_norm_eps     = 1e-5    .
+    dropout            = 0.1     ,
+    checkpoint_every   = True    
+)
+
+assert config.norm_order in ['post', 'pre']

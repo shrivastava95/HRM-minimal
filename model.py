91d271f62103deee4ad18e0451218a53258bd5a9
Ishaan Shrivastava <ishaanshri95@gmail.com>
2025-08-23 18:08:04 +0530

    add inner HRM loop

--- model.py
+++ model.py
@@ -2,10 +2,13 @@ import os
 import sys
 import math
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Tuple, List, Dict, Optional
 
 import torch
 from torch import nn
 import torch.nn.functional as F
+from common import trunc_normal_init_
 
 from config import config
 
@@ -21,9 +24,11 @@ class RotaryEmbedding(nn.Module):
         inv = 1.0 / (base ** (torch.arange(0, dim, 2, device=device, dtype=dtype) / dim))
         t = torch.arange(max_position_embeddings, device=device, dtype=dtype)
         freqs = torch.outer(t, inv)                               # [T, D/2]
-        emb = torch.cat((freqs, freqs), dim=-1)                   # [T, D]
-        self.register_buffer("cos_cached", emb.cos(), persistent=False)
-        self.register_buffer("sin_cached", emb.sin(), persistent=False)
+        # emb = torch.cat((freqs, freqs), dim=-1)                   # [T, D]
+        # rotate nahi karna hai. downstream fully vectorized _apply_rope me useful hoga but we used slicing instead.
+        
+        self.register_buffer("cos_cached", freqs.cos())
+        self.register_buffer("sin_cached", freqs.sin())
 
     def forward(self, seq_len: int, dtype=None):
         cos = self.cos_cached[:seq_len]
@@ -35,24 +40,29 @@ class RotaryEmbedding(nn.Module):
 
 
 class MultiHeadSelfAttentionWithRoPE(nn.Module):
-    def __init__(self, config, device=None)
+    def __init__(self, config, device=None):
         super().__init__()
         self.dim = config.d_model
         self.n_heads = config.num_heads
-        self.d = self.dim // n_heads
+        self.d = self.dim // self.n_heads
         self.causal = config.causal
         self.dropout = config.dropout
         self.rope_theta = config.rope_theta
 
         self.rope = RotaryEmbedding(config, device)
-        self.qkv = nn.Linear(dim, dim * 3, bias = False)
-        self.out = nn.Linear(dim, dim, bias = False)
-        self.attn_drop = nn.Dropout(dropout)
-        self.proj_drop = nn.Dropout(dropout)
+        self.qkv = nn.Linear(self.dim, self.dim * 3, bias = False)
+        self.out = nn.Linear(self.dim, self.dim, bias = False)
+        self.attn_drop = nn.Dropout(self.dropout)
+        self.proj_drop = nn.Dropout(self.dropout)
     
     
     def _to_heads(self, q, k, v):
-        q, k, v = [item.view(B, T, self.h, self.d).transpose(1, 2) for item in (q, k, v)] # [B, H, T, D]
+        B, T, C = q.shape
+        q, k, v = [
+            item.view(B, T, self.n_heads, self.d).transpose(1, 2)
+            for item 
+            in (q, k, v)
+        ] # [B, H, T, D]
         return q, k, v
 
 
@@ -63,7 +73,7 @@ class MultiHeadSelfAttentionWithRoPE(nn.Module):
         """
         # split even/odd channels
         x_even = x[..., ::2]  # [B,H,T,D/2]
-        x_odd  = x[..., 1::2] # [B,H,T,D/2]
+        x_odd  = x[..., 1::2] # [B,H,T,D/2] 
 
         # broadcast cos/sin to [1,1,T,D/2]
         cos = cos.unsqueeze(0).unsqueeze(0)
@@ -77,8 +87,26 @@ class MultiHeadSelfAttentionWithRoPE(nn.Module):
         out[..., ::2] = x_even_rot
         out[..., 1::2] = x_odd_rot
         return out
-
-
+    
+    
+    def sdpa(self, q, k, v, causal=False):
+        B, H, T, D = k.shape
+        scale = 1.0 / math.sqrt(self.d)
+        att = torch.matmul(
+            q,                  # [B, H, T, D] ... 
+            k.transpose(-2, -1) # ... @ [B, H, D, T] -> [B, H, T, T]
+        ) * scale 
+        if causal:
+            mask = torch.ones(T, T, device=self.device, dtype=torch.bool).triu(1) 
+            # [ [0, 1],  q1
+            #   [0, 0] ] q2
+            #   k1  k2
+            # queries do not see future keys! 1s are filled with -inf
+            att = att.masked_fill(mask, float("-inf"))
+        att = self.attn_drop(F.softmax(att, dim=-1)) # [B, H, T, T]
+        o = torch.matmul(att, v) # [B, H, T, T] @ [B, H, T, D] -> [B, H, T, D]
+        return o
+        
     def forward(self, x, dtype=None):
         B, T, C = x.shape
         qkv = self.qkv(x) # [B, T, C] -> [B, T, 3C]
@@ -93,15 +121,13 @@ class MultiHeadSelfAttentionWithRoPE(nn.Module):
         k = self._apply_rope(k, cos, sin)
 
         # scaled dot-product attention
-        scale = 1.0 / math.sqrt(self.d)
-        att = torch.matmul(q, k.transpose(-2, -1)) * scale # [B, H, T, D] @ [B, H, D, T] -> [B, H, T, T]
-        if self.causal: # mask the upper triangular elements?
-            mask = torch.ones(T, T, device=self.device, dtype=torch.bool).triu(1)
-            att = att.masked_fill(mask, float("-inf"))
-        att = F.softmax(att, dim=-1) # softmax across the keys dimension to get P. each query gets mapped to P * values
-        att = self.attn_drop(att)
-        o = torch.matmul(att, v) # [B, H, T, T] @ [B, H, T, D] -> [B, H, T, D]
+        o = self.sdpa(q, k, v, self.causal)
 
+        # BTC se BTHD view se gaye.
+        # fir
+        # BTHD se BTC view se wapas aaye.
+        # LMAOOO
+        
         o = o.transpose(1, 2).contiguous().view(B, T, C) # back to [B, T, C]
         o = self.out(o)
         o = self.proj_drop(o)
@@ -147,7 +173,8 @@ class HRMBlock(nn.Module):
             self.act = nn.GELU() if self.act_kind == "gelu" else nn.SiLU()
         self.drop = nn.Dropout(p)
 
-    def forward(self, x):
+
+    def forward(self, x): # crazy forward pass, need to refactor whole class and make this forward pass more readable.
         # --- norms
         if self.norm_kind == "rms":
             def rmsnorm(x, w):
@@ -190,18 +217,184 @@ class HRMRecurrentModule(nn.Module):
             hidden_states = hidden_states + additional_input
         
         for _layer in self.layers:
-            hidden_states = _layers(hidden_states)
+            hidden_states = _layer(hidden_states)
         
         return hidden_states
 
-class HRM(nn.Module):
+
+@dataclass
+class InnerCarry(nn.Module):
+    Z_L: torch.Tensor
+    Z_H: torch.Tensor
+
+
+@dataclass
+class Carry(nn.Module):
+    inner_carry: InnerCarry
+    halted: torch.Tensor
+    steps: torch.Tensor
+    current_data: Dict[str, torch.Tensor]
+
+
+class HRM_Inner(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.num_puzzle_embs = config.num_puzzle_embs
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.d_model)
+        self.embed_tokens.weight = nn.Parameter(trunc_normal_init_(torch.empty(config.vocab_size, config.d_model), std=math.sqrt(config.d_model)))
         self.L_module = HRMRecurrentModule([HRMBlock(config, MultiHeadSelfAttentionWithRoPE(config)) for _i in range(config.T_L)])
         self.H_module = HRMRecurrentModule([HRMBlock(config, MultiHeadSelfAttentionWithRoPE(config)) for _i in range(config.H_L)])
+        self.L_cycles = config.L_cycles
+        self.H_cycles = config.H_cycles
+        self.M_min, self.M_max = config.M_min, config.M_max
+        self.register_buffer("Z_L_init", trunc_normal_init_(torch.empty(self.config.d_model, dtype=torch.float32), std=1))
+        self.register_buffer("Z_H_init", trunc_normal_init_(torch.empty(self.config.d_model, dtype=torch.float32), std=1))
+        self.lm_head = nn.Linear(self.config.d_model, self.config.vocab_size - 1, bias=False)
+        self.q_head  = nn.Linear(self.config.d_model, 2, bias=True)
+        for linearlayer in [self.lm_head, self.q_head]:
+            linearlayer.weight = nn.Parameter(
+                trunc_normal_init_(
+                    tensor = torch.zeros_like(linearlayer.weight), 
+                    std = 1.0 / math.sqrt(linearlayer.weight.shape[-1])
+                )
+            )
+            if hasattr(linearlayer, "bias") and linearlayer.bias is not None:
+                linearlayer.bias = nn.Parameter(torch.zeros_like(linearlayer.bias))
+            
+        # ek batch ke across halted ko track kiya, for multiple values of M
+        # M= 1    2    3    4 (M_max)
+        #    0 -> 0 -> 1 -> no forward pass (shuru me hi exit the forward())
+        #    0 -> 0 -> 0 -> 1 -> no forward pass
+        #    0 -> 0 -> 1 -> no forward pass
+        #    0 -> 0 -> 0 -> 1 -> no forward pass
+    
 
-    def forward(self, input_embeds):
+    def get_init_inner_carry(self, batch_size, max_seq_len) -> InnerCarry:
+        return InnerCarry(
+            self.Z_L_init.unsqueeze(0).unsqueeze(0).repeat(batch_size, max_seq_len, 1), 
+            self.Z_H_init.unsqueeze(0).unsqueeze(0).repeat(batch_size, max_seq_len, 1),
+        )
+    
+    
+    def reset_inner_carry(self, reset_flag: torch.Tensor, inner_carry: InnerCarry) -> InnerCarry:
+        """
+        For sequences that are halted, reset the carry.
+        """
+        # batch has:
+        # 1. inputs: [B, 81]
+        # 2. labels: [B, 81]
+        # 3. puzzle_identifiers: [B]
+        # carry ka halted use karke torch.where se fresh batch reset karna hai.
+        # remember that a carry has these things: inner_carry, halted, current_data.
+        batch_size = inner_carry.Z_L.shape[0]
+        
+        init_inner_carry = self.get_init_inner_carry(batch_size, config.max_seq_len)
+        # new_carry = Carry(
+        #     inner_carry = InnerCarry(
+        #         Z_L = torch.where(reset_flag.reshape(-1, 1, 1), init_inner_carry.Z_L, inner_carry.Z_L),
+        #         Z_H = torch.where(reset_flag.reshape(-1, 1, 1), init_inner_carry.Z_H, inner_carry.Z_H)
+        #     ),
+        #     halted = torch.zeros(batch_size, dtype=torch.bool),
+        ######## note that the line below will never work because it doesnt respect the shape of batch[k]
+        #     current_data = {k: torch.where(reset_flag.reshape(-1, 1, 1), batch[k], inner_carry.current_data[k]) for k in batch.keys()}
+        # )
+        new_inner_carry = InnerCarry(
+            Z_L = torch.where(reset_flag.reshape(-1, 1, 1), init_inner_carry.Z_L, inner_carry.Z_L),
+            Z_H = torch.where(reset_flag.reshape(-1, 1, 1), init_inner_carry.Z_H, inner_carry.Z_H)
+        )
+        
+        return new_inner_carry
 
+
+    def get_input_embeds(self, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
+        # get the puzzle embeds from the puzzle_identifiers.
+        puzzle_token_ids = batch["puzzle_identifiers"]
+        # get the puzzle token embeds from the embed_tokens.
+        puzzle_input_ids = batch["inputs"]
+        # concatenate the puzzle token ids and the puzzle input ids.
+        puzzle_token_ids = torch.cat([
+            puzzle_token_ids.unsqueeze(-1), 
+            puzzle_input_ids    
+        ])
+        # get the emebds for the concatenated token seqence.
+        input_embeds = self.embed_tokens(puzzle_token_ids)
+        return input_embeds
+    
+    
+    def forward(self, inner_carry: InnerCarry, batch: Dict[str, torch.Tensor]) -> Tuple[InnerCarry, torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        """
+        Input:
+        - carry: InnerCarry
+        Output:
+        - new_inner_carry: InnerCarry
+        - output_token_embeds: torch.Tensor of shape [B, T-1, D]
+        - (q_halt_logits, q_continue_logits): Tuple[torch.Tensor of shape [B, D], torch.Tensor of shape [B, D]]
+        """
+        
+        # Input encoding
+        # for our case, this is just an nn.Embedding layer
+        # first, get embeddings for the joint token sequence
+        input_embeds = self.get_input_embeds(batch)
+        
+        # init Z_L and Z_H
+        Z_H, Z_L = inner_carry.Z_H, inner_carry.Z_L
+        
+        # then, run one forward pass (apart from the last iter)
+        with torch.no_grad():
+            for _Hiter in range(self.H_cycles):
+                for _Liter in range(self.L_cycles):
+                    if not ((_Hiter + 1 == self.H_cycles) and (_Liter + 1 == self.L_cycles)):
+                        Z_L = self.L_module(Z_L, Z_H + input_embeds)
+                if not (_Hiter + 1 == self.H_cycles):
+                    Z_H = self.H_module(Z_H, Z_L)
+        
+        Z_L = self.L_module(Z_L, Z_H + input_embeds)
+        Z_H = self.H_module(Z_H, Z_L)
+        
+        # detach the gradients for the next forward pass.
+        new_inner_carry = InnerCarry(
+            Z_L.detach(),
+            Z_H.detach()
+        )
+        
+        # finally, run through the entire stack for inference.                
+        output_token_embeds = self.lm_head(Z_H)[:, self.config.num_puzzle_embs:]
+        self.q_continue
+        # # q_head (for now its unused, disabled)
+        # q_head. we are switching back to using it. hopefully it works. if god is willing.
+        
+        q_logits = self.q_head(Z_H[:, 0]).to(torch.float32)
+        q_halt_logits, q_continue_logits = q_logits[..., 1], q_logits[..., 0]
+        # q_halt_logits, q_continue_logits = None, None
+        
+        return new_inner_carry, output_token_embeds, (q_halt_logits, q_continue_logits)
+        
+        
+class HRM_model(nn.Module):
+    def __init__(self, config):
+        self.config = config
+        self.inner = HRM_Inner(config)
+        assert False, "Not Implemented yet"
+    
+    
+    def initial_carry(self, batch: Dict[str, torch.Tensor]) -> Carry:
+        batch_size = batch["inputs"].shape[0]
+        # ... for the whole batch now
+        halted = torch.ones(batch_size, dtype=torch.bool).to(batch["inputs"].dtype) 
+        # all halted, will be reset on the first forward pass.
+        current_data = {k: v for k, v in batch.items()}
+        inner_carry = self.get_init_inner_carry(batch_size, config.max_seq_len)
+        return Carry(inner_carry, halted, current_data)
+        
+    def forward(self, carry: Carry) -> Tuple[Carry, torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]: 
+        # we need to reset the carry for all halted sequences,
+        # and then forward all the elements of the batch.
+        
+        # first, we initialize the carry by resetting the halted sequences.
+        new_inner_carry = carry.inner.reset_carry(carry)
+        assert False, "Not Implemented"
+        
+                
 
 
 def main():
